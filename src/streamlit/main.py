ac8de5a0913891db66ceb62ebac7532b476a74c1
Hossam Eid <hossameid752@gmail.com>
2026-03-07 00:01:04 +0300

    Добавлен каркас сервера Streamlit (в разработке)

---
+++ src/streamlit/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+# TODO: Имплементация API для инференса моделей (в разработке)
+st.title("Breast Cancer AI Diagnostics API")
+st.write("Сервер находится в стадии разработки (Work in Progress)...")
